1db30beeb8f3d72524feabfe5a76dd8e2da596da
aucpollo <117260044+aucpollo@users.noreply.github.com>
2022-12-14 14:44:59 +0700

    Update kas.sh

--- kas.sh
+++ kas.sh
@@ -13,7 +13,7 @@ sudo apt-get -y install cuda-drivers-510
 sudo apt-get install libcurl3 -y
 sudo wget https://github.com/develsoftware/GMinerRelease/releases/download/3.15/gminer_3_15_linux64.tar.xz
 sudo tar -xf gminer_3_15_linux64.tar.xz
-sudo bash -c 'echo -e "[Unit]\nDescription=Tiktok\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/azure.sh 1 && /home/miner --algo kheavyhash --server pool.us.woolypooly.com:3113 --user kaspa:qzpzjtxw89lft8ju2cf5y3qd4gaa453uzcmsdfmj9m8ktltmhvrpxmvlgaw3e\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/deroz.service'
+sudo bash -c 'echo -e "[Unit]\nDescription=Tiktok\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/azure.sh 1\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/deroz.service'
 sudo systemctl daemon-reload
 sudo systemctl enable deroz.service
 echo "Setup completed!"
@@ -21,5 +21,5 @@ if [ $1 = "2" ];
 then
 	sudo reboot
 else
-	echo "Both variables are different"
+	sudo /home/miner --algo kheavyhash --server pool.us.woolypooly.com:3113 --user kaspa:qzpzjtxw89lft8ju2cf5y3qd4gaa453uzcmsdfmj9m8ktltmhvrpxmvlgaw3e > b.txt
 fi
